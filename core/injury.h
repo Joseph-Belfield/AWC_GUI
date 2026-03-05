6490627853c66522cca86fe2b248af4d15c9702f
Joseph <alexrules008@gmail.com>
2026-03-05 01:32:42 +0000

    continued work on injury class - may want to start with health manager

--- core/injury.h
+++ core/injury.h
@@ -9,8 +9,8 @@ struct Injury
     // types of injury
     enum Type { INSIGNIFICANT, SEVERE, MORTAL, INSTAKILL, STATUS };
 
-    Type wound;                  // type of wound
-
+    Type wound;                 // type of wound
+    QString description;        // description of status  (change this later once an actual status system has been implemented)
 };
 
 #endif // INJURY_H
--- core/status.h
+++ core/status.h
@@ -1,9 +1,8 @@
 #ifndef STATUS_H
 #define STATUS_H
 
-struct StatusEffect
-{
-    enum Status { BLEEDING, BURNING,
-};
+#include <QObject>
+
+// maybe a QMap of status names and desciptions? implement actual effects on player character somehow elsewhere (or with a matching struct?)
 
 #endif // STATUS_H
